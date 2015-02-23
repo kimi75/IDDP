f51e2b87dc27063ea143828de9691371b16c0d45
kimi75 <hlahmar@gmail.com>
2015-02-23 19:28:59 +0100

    Initial Commit

---
+++ opencv3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencv3
+//
+//  Created by hakim on 23/02/2015.
+//  Copyright (c) 2015 hakim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
